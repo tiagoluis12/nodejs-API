3ca0ca020fed3ddfaf2b067486aa241bcc32c9ce
“tiagoluis12” <tiago7164@hotmail.com>
2023-07-17 23:17:28 +0100

    status code implementation inside the API

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,21 +7,41 @@ const baseUrl = "http://api.weatherapi.com/v1";
 const currentMethod = "/current.json";
 
 userRouter.get("/", async (request, response) => {
-  const dataCity = await fetch(
-    `${baseUrl}${currentMethod}?` +
-      new URLSearchParams({
-        key: process.env.NODE_API_KEY,
-        q: request.body.cidade,
-      })
-  );
+  try {
+    if (!request.body.cidade || typeof request.body.cidade != "string") {
+      response.status(400).send({
+        success: false,
+        message: "Não existe uma cidade no corpo da requisição",
+      });
+      return;
+    }
 
-  const dataResponse = await dataCity.json();
-  const responseString = `A temperatura da ${dataResponse.location.name} buscada é: ${dataResponse.current.temp_c}`;
-  response.status(200).send(responseString);
+    const dataCity = await fetch(
+      `${baseUrl}${currentMethod}?` +
+        new URLSearchParams({
+          key: process.env.NODE_API_KEY,
+          q: `${request.body.cidade}`,
+        })
+    );
+
+    const dataResponse = await dataCity.json();
+    if (dataResponse.error) {
+      response.status(404).send({
+        success: false,
+        message: dataResponse.error.message,
+      });
+    }
+
+    const responseString = `A temperatura da ${dataResponse.location.name} buscada é: ${dataResponse.current.temp_c}`;
+    response.status(200).send({
+      success: true,
+      message: responseString,
+    });
+  } catch (error) {
+    response.status(500).send({
+      success: false,
+      message: `Erro no servidor: ${error}`,
+    });
+  }
 });
-
 module.exports = { userRouter };
-// pocess.env.NODE_API_KEY
-// 0739b76521c74aeda07202903231307
-
-// ?key=0739b76521c74aeda07202903231307&q=Galway
