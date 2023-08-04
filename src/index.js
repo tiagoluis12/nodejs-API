36e6928e8dd403b6a8accc9e5fb51f7efa4b6f39
“tiagoluis12” <tiago7164@hotmail.com>
2023-08-04 14:26:40 +0100

    fixing request failure when trying to check states temperature

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userRouter } = require("./routes/userRoutes");
 const app = express();
+const axios = require("axios");
 require("dotenv").config();
 
 console.log(process.env.NODE_API_KEY);
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -36,6 +36,8 @@ userRouter.get("/", async (request, response) => {
     response.status(200).send({
       success: true,
       message: responseString,
+      maxTemperature: dataResponse.forecast.forecastday[0].day.maxtemp_c, //aqui
+      minTemperature: dataResponse.forecast.forecastday[0].day.mintemp_c, //aqui
     });
   } catch (error) {
     response.status(500).send({
@@ -44,4 +46,109 @@ userRouter.get("/", async (request, response) => {
     });
   }
 });
+
+userRouter.get("/capitais", async (request, response) => {
+  try {
+    const capitais = [
+      { name: "Rio Branco" },
+      { name: "Maceió" },
+      { name: "Macapá" },
+      { name: "Manaus" },
+      { name: "Salvador" },
+      { name: "Fortaleza" },
+      { name: "Brasília" },
+      { name: "Vitória" },
+      { name: "Goiânia" },
+      { name: "São Luís" },
+      { name: "Cuiabá" },
+      { name: "Campo Grande" },
+      { name: "Belo Horizonte" },
+      { name: "Belém" },
+      { name: "João Pessoa" },
+      { name: "Curitiba" },
+      { name: "Recife" },
+      { name: "Teresina" },
+      { name: "Rio de Janeiro" },
+      { name: "Natal" },
+      { name: "Porto Alegre" },
+      { name: "Porto Velho" },
+      { name: "Boa Vista" },
+      { name: "Florianópolis" },
+      { name: "São Paulo" },
+      { name: "Aracaju" },
+      { name: "Palmas" },
+    ];
+
+    const dataPromises = capitais.map(async (capital) => {
+      const dataCity = await fetch(
+        `${baseUrl}${currentMethod}?` +
+          new URLSearchParams({
+            key: process.env.NODE_API_KEY,
+            q: `${capital.name}`,
+          })
+      );
+
+      const dataResponse = await dataCity.json();
+      if (dataResponse.error) {
+        response.status(404).send({
+          success: false,
+          message: dataResponse.error.message,
+        });
+      }
+
+      return {
+        name: capital.name,
+        temperatura: dataResponse.current.temp_c,
+        maxTemperature: dataResponse.forecast.forecastday[0].day.maxtemp_c, //aqui
+        minTemperature: dataResponse.forecast.forecastday[0].day.mintemp_c, //aqui
+      };
+    });
+
+    const result = await Promise.all(dataPromises);
+
+    response.status(200).send({
+      success: true,
+      message: result,
+    });
+  } catch (error) {
+    response.status(500).send({
+      success: false,
+      message: `Erro no servidor: ${error}`,
+    });
+  }
+});
+// Lista de capitais para consulta
+// const capitais = [
+//   { nome: "Rio de Janeiro", id: 3451190 },
+//   { nome: "São Paulo", id: 3448439 },
+//   { nome: "Brasília", id: 3469058 },
+// ];
+
+// userRouter.get("/capitais", async (request, response) => {
+//   try {
+//     const dataPromises = capitais.map(async (capital) => {
+//       const response = await fetch("http://api.weatherapi.com/v1/current.json");
+//       const temperaturaMaxima =
+//         response.data.forecast.forecastday[0].day.maxtemp_c;
+//       const temperaturaMinima =
+//         response.data.forecast.forecastday[0].day.mintemp_c;
+//       const temperaturaMedia = (temperaturaMaxima + temperaturaMinima) / 2;
+
+//       return {
+//         nome: capital.nome,
+//         temperaturaMaxima,
+//         temperaturaMinima,
+//         temperaturaMedia,
+//       };
+//     });
+
+//     const result = await Promise.all(dataPromises);
+
+//     res.json(result);
+//   } catch (error) {
+//     console.error("Erro ao obter dados:", error.message);
+//     res.status(500).json({ error: "Erro ao obter dados." });
+//   }
+// });
+
 module.exports = { userRouter };
